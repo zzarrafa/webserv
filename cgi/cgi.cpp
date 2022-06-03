9d85479c8a65d5ead71bbc554acc8163606c1b3e
Souhail El fchtali <sel-fcht@e2r1p6.1337.ma>
2022-06-03 08:49:48 +0100

    changes

--- cgi/cgi.cpp
+++ cgi/cgi.cpp
@@ -1,10 +1,30 @@
 #include "cgi.hpp"
 
+
 std::pair<std::string, std::map<std::string, std::string> > Cgi::execution(std::string path, request &req, std::string cgipathto)
 {  
-    std::cout << " HIHIHIHIHI " << req.get_header("Content-type") << std::endl;
-    //if(req.get_method() == "POST")
-      //  if (!req.get_header("Content-type").empty())
-        
+    std::cout << "  " << req.get_header("Content-type") << std::endl;
+    if(req.get_method() == "POST")
+    {
+      if (!req.get_header("Content-Type").empty())
+        setenv("CONTENT_TYPE", req.get_header("Content-Type").c_str(),1);
+    }
+    if(!req.get_body().empty())
+      setenv("CONTENT_LENGTH", std::to_string(req.get_body().size()).c_str(),1);
+    if (!req.get_method().empty())
+      setenv("REQUEST_METHOD",req.get_method().c_str(),1 );
+    setenv("REDIRECT_STATUS", "true" , 1);
+    setenv("SCRIPT_FILENAME", path.c_str(), 1);
+    setenv("GATEWAY_INTERFACE", "CGI/1.1", 1);
+    setenv("HTTP_ACCEPT_ENCODING","",0);
+    setenv("HTTP_ACCEPT", "" ,0);
+    setenv("HTTP_ACCEPT_LANGUAGE","en-US,en;q=0.9",0);
+    //bonus
+    if (!req.get_header("cookie").empty())
+      setenv("cookie", req.get_header("cookie").c_str(),1);
+    setenv("SERVER_PROTOCOL","HTTP/1.1",0);
+    setenv("REQUEST_SCHEME","http",0);
+}
+
 
 }
